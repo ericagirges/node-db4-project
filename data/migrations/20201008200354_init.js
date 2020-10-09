e2b8e38215491b20c3b51cb375684a4fdefcc4df
Erica Girges <ericafiorenza@gmail.com>
2020-10-08 22:13:28 -0700

    migrations edit

--- data/migrations/20201008200354_init.js
+++ data/migrations/20201008200354_init.js
@@ -8,16 +8,10 @@ exports.up = function (knex) {
       tbl.increments();
       tbl.string("name").notNUllable().unique().index();
     })
-    .createTable("RecipeIngredients", (tbl) => {
+    .createTable("Steps", (tbl) => {
       tbl.increments();
-      tbl.string("name").notNUllable();
-      tbl.string("quantity").notNUllable();
-      tbl
-        .integer("ingredient_id")
-        .unsigned()
-        .references("Ingredients.id")
-        .onDelete("RESTRICT")
-        .onUpdate("CASCADE");
+      tbl.string("content").notNUllable();
+      tbl.integer("step_order").notNUllable();
       tbl
         .integer("recipe_id")
         .unsigned()
@@ -25,26 +19,29 @@ exports.up = function (knex) {
         .onDelete("RESTRICT")
         .onUpdate("CASCADE");
     })
-    .createTable("Steps", (tbl) => {
-      tbl.increments();
-      tbl.string("step_one").notNUllable();
-      tbl.string("step_two");
-      tbl.string("step_three");
-      tbl.string("step_four");
-      tbl.string("step_five");
-      tbl
-        .integer("recipeingredient_id")
-        .unsigned()
-        .references("RecipeIngredients.id")
-        .onDelete("RESTRICT")
-        .onUpdate("CASCADE");
-      tbl
-        .integer("recipe_id")
-        .unsigned()
-        .references("Recipes.id")
-        .onDelete("RESTRICT")
-        .onUpdate("CASCADE");
-    });
+    .createTable("RecipeIngredients", (tbl) => {
+        tbl.increments();
+        tbl.string("name").notNUllable();
+        tbl.string("quantity").notNUllable();
+        tbl
+          .integer("ingredient_id")
+          .unsigned()
+          .references("Ingredients.id")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+        tbl
+          .integer("recipe_id")
+          .unsigned()
+          .references("Recipes.id")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+          tbl
+          .integer("step_id")
+          .unsigned()
+          .references("Steps.id")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+      })
 };
 
 exports.down = function (knex) {
